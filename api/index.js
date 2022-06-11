633af853188a8c9112af0315edb2a6f9de9c56d8
Shahd Khattab <83475739+shahdkhattab@users.noreply.github.com>
2022-06-11 17:35:15 +0200

    final

--- api/index.js
+++ api/index.js
@@ -28,11 +28,11 @@ app.post('/api/payments', async (req,res) => {
       success: true,
       message: 'Payment Successful',
       id: charge.id,
-      /*
-      notification: await axios.post("https://notification-service-daradeer.vercel.app/api/notification",{
+      
+      notification: await axios.post("https://notification-service.vercel.app/api/notification",{
         email,
         text: "Payment Successful"
-      })*/
+      })
     });
   } catch(error) {
   console.log("Error ", error)
