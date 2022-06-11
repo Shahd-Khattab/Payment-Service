a4d644f1b6e4096d289686281442f32653c75e69
Shahd Khattab <83475739+shahdkhattab@users.noreply.github.com>
2022-06-11 17:49:38 +0200

    a

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const bodyParser = require('body-parser')
 const express = require('express');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const axios= require('axios');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
